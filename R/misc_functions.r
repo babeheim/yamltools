7ac288b56018b795916bfaefaebced2876c56875
Bret Beheim <beheim@gmail.com>
2017-09-25 14:52:24 +0200

    more debugging, testing, revising

--- R/misc_functions.r
+++ R/misc_functions.r
@@ -4,9 +4,8 @@ nullToNA <- function(x) {
   return(x)
 }
 
-
-yaml_to_json <- function(file){
-  x <- readLines( file )
+read_yaml <- function(file, to.json=FALSE){
+  x <- readLines( file , warn=FALSE )
   x <- gsub("\"", "'", x)
   for(i in 1:length(x)){
     if( length(grep(": ", x[i])) > 0 & length(grep(": $", x[i])) == 0 ){
@@ -15,51 +14,20 @@ yaml_to_json <- function(file){
     }
   }
   if(x[length(x)] != "") x <- c(x, "")
-  new <- yaml.load(paste(x, collapse="\n"))
-  output <- jsonlite::toJSON(new, pretty=TRUE)
-  newname <- gsub("\\.yaml|\\.yml", ".json", file)
-  writeLines(output, newname)
+  output <- yaml.load(paste(x, collapse="\n"))
+  if(to.json) output <- jsonlite::toJSON(output, pretty=TRUE)
+  return(output)
 }
-
-# rbind_plus <- function( df1, df2 ){
-#   df1 <- do.call(data.frame, df1)
-#   df2 <- do.call(data.frame, df2)
-#   n1 <- colnames(df1)
-#   n2 <- colnames(df2)
-#   if( any(!n1 %in% n2) ){
-#     new_cols <- n1[!n1 %in% n2]
-#     df2 <- cbind(df2, matrix("", nrow=nrow(df2), ncol=length(new_cols)))
-#     colnames(df2) <- c(n2, new_cols)
-#   }
-#   if( any(!n2 %in% n1) ){
-#     new_cols <- n2[!n2 %in% n1]
-#     df1 <- cbind(df1, matrix("", nrow=nrow(df1), ncol=length(new_cols)))
-#     colnames(df1) <- c(n1, new_cols)
-#   }
-#   output <- rbind(df1, df2)
-#   output
-# }
 
 vectorize <- function( input_list ){
   for(i in 1:ncol(input_list)){
     if(class(input_list[,i])=="list"){
-      empty <- which(lapply(input_list[,i], class)=="list")
-      if(length(empty)>0) input_list[empty,i] <- NA
-      input_list[,i] <- unlist(nullToNA(input_list[,i]))
+      if(class(input_list[,i][[1]])!="data.frame"){
+        empty <- which(lapply(input_list[,i], class)=="list")
+        if(length(empty)>0) input_list[empty,i] <- NA
+        input_list[,i] <- unlist(nullToNA(input_list[,i]))
+      }
     }
   }
   input_list
-}
-
-list_to_dataframe <- function( input_list ){
-    column_names <- unique(unlist(lapply(input_list, names)))
-    output <- matrix( "", nrow = length(input_list), ncol=length(column_names) )
-    colnames(output) <- column_names
-    output <- as.data.frame( output )
-    for(j in 1:ncol(output)) output[,j] <- as.character(output[,j]) 
-    for(i in 1:length(input_list)){
-        new_record <- unlist(input_list[[i]])
-        output[i, names(new_record)] <- new_record 
-    }
-    output
 }
--- tests/test_yamltools.r
+++ tests/test_yamltools.r
@@ -1,9 +1,11 @@
-
+rm(list=ls())
 
 test_that("works on all of them", {
   my_files <- list.files('./single_yaml_to_json', pattern="\\.yml|\\.yaml", full.names=TRUE)
   for(i in 1:length(my_files)){
-    yaml_to_json(my_files[i])
+    x <- read_yaml(my_files[i], to.json=TRUE)
+    newname <- gsub("\\.yaml|\\.yml", ".json", my_files[i])
+    writeLines(x, newname)
   }
   my_files <- list.files('./single_yaml_to_json', pattern="\\.json", full.names=TRUE)
   expect_true(length(my_files)==4)
@@ -11,61 +13,121 @@ test_that("works on all of them", {
 
 test_that("flat array", {
   my_file <- "./single_yaml_to_json/flat_array.yml" 
-  yaml_to_json(my_file)
+  x <- read_yaml(my_file, to.json=TRUE)
+  newname <- gsub("\\.yaml|\\.yml", ".json", my_file)
+  writeLines(x, newname) 
   my_file <- "./single_yaml_to_json/flat_array.json" 
   x <- read_json(my_file, simplifyVector=TRUE)
+  x <- vectorize(x)
   expect_true(all(dim(x)==c(3,3)))
 })
 
 test_that("flat array with unique keys", {
   my_file <- "./single_yaml_to_json/flat_array_unique_keys.yaml" 
-  yaml_to_json(my_file)
+  x <- read_yaml(my_file, to.json=TRUE)
+  newname <- gsub("\\.yaml|\\.yml", ".json", my_file)
+  writeLines(x, newname) 
   my_file <- "./single_yaml_to_json/flat_array_unique_keys.json" 
   x <- read_json(my_file, simplifyVector=TRUE)
+  x <- vectorize(x)
   expect_true(all(dim(x)==c(3,5)))
 })
 
 test_that("one_subtable", {
   my_file <- "./single_yaml_to_json/one_subtable.yaml" 
-  yaml_to_json(my_file)
+  x <- read_yaml(my_file, to.json=TRUE)
+  newname <- gsub("\\.yaml|\\.yml", ".json", my_file)
+  writeLines(x, newname) 
   my_file <- "./single_yaml_to_json/one_subtable.json" 
   x <- read_json(my_file, simplifyVector=TRUE)
+  x <- vectorize(x)
   expect_true(all(dim(x)==c(3,2)))
   expect_true(length(x$entries)==3)
 })
 
 test_that("n_subtables", {
   my_file <- "./single_yaml_to_json/n_subtables.yaml" 
-  yaml_to_json(my_file)
+  x <- read_yaml(my_file, to.json=TRUE)
+  newname <- gsub("\\.yaml|\\.yml", ".json", my_file)
+  writeLines(x, newname) 
   my_file <- "./single_yaml_to_json/n_subtables.json" 
   x <- read_json(my_file, simplifyVector=TRUE)
   expect_true(all(dim(x)==c(3,4)))
+  x <- vectorize(x)
   expect_true(length(x$residents)==3)
   expect_true(length(x$visitors)==3)
 })
 
-test_that("can combine simple yamls into one dataframe", {
+test_that("can combine flat array yamls with identical structures into one dataframe", {
   my_files <- list.files('./batch_yaml_flat', pattern="*.yml", full.names=TRUE)
-  lapply(my_files, yaml_to_json)
+  for(i in 1:length(my_files)){
+    x <- read_yaml(my_files[i], to.json=TRUE)
+    newname <- gsub("\\.yaml|\\.yml", ".json", my_files[i])
+    writeLines(x, newname) 
+  }
   my_files <- gsub("\\.*yml", ".json", my_files)
   output <- lapply(my_files, function(z) read_json(z, simplifyVector=TRUE))
   expect_true(all(unlist(lapply(output, class)) == "data.frame"))
   # rbind.pages combines a list of dataframes
-  output <- jsonlite::rbind.pages(output)
+  output <- jsonlite::rbind_pages(output)
+  output <- vectorize(output)
   expect_true(all(dim(output) == c(27,3)))
 })
 
 # is rbind.pages saavy enough if they have different shapes tho? yes! goodbye rbind_plus!
 # apparently plry::rbind.fill does this job too
 
-test_that("can combine variable-column yamls into one dataframe", {
+test_that("can combine flat-array yamls with unique keys into one dataframe", {
   my_files <- list.files('./batch_yaml_flat_variable', pattern="*.yml", full.names=TRUE)
-  lapply(my_files, yaml_to_json)
-  my_files <- gsub("\\.*yml", ".json", my_files)
+  for(i in 1:length(my_files)){
+    x <- read_yaml(my_files[i], to.json=TRUE)
+    newname <- gsub("\\.yaml|\\.yml", ".json", my_files[i])
+    writeLines(x, newname) 
+  }
+  my_files <- gsub("\\.yml", ".json", my_files)
   output <- lapply(my_files, function(z) read_json(z, simplifyVector=TRUE))
   expect_true(all(unlist(lapply(output, class)) == "data.frame"))
   # rbind.pages combines a list of dataframes
-  output <- jsonlite::rbind.pages(output)
+  output <- jsonlite::rbind_pages(output)
+  output <- vectorize(output)
   expect_true(all(dim(output) == c(27,4)))
 })
+
+# now multiple files with structures!
+
+test_that("can combine one-subtable yamls into one dataframe", {
+  my_files <- list.files('./batch_yaml_one_subtable', pattern="*.yaml", full.names=TRUE)
+  for(i in 1:length(my_files)){
+    x <- read_yaml(my_files[i], to.json=TRUE)
+    newname <- gsub("\\.yaml|\\.yml", ".json", my_files[i])
+    writeLines(x, newname) 
+  }
+  my_files <- gsub("\\.yaml", ".json", my_files)
+  output <- lapply(my_files, function(z) read_json(z, simplifyVector=TRUE))
+  expect_true(all(unlist(lapply(output, class)) == "data.frame"))
+  # rbind.pages combines a list of dataframes
+  output <- jsonlite::rbind_pages(output)
+  output <- vectorize(output)
+  expect_true(all(dim(output) == c(21,2)))
+})
+
+test_that("can combine n-subtable yamls into one dataframe", {
+  my_files <- list.files('./batch_yaml_n_subtables', pattern="*.yaml", full.names=TRUE)
+  for(i in 1:length(my_files)){
+    x <- read_yaml(my_files[i], to.json=TRUE)
+    newname <- gsub("\\.yaml|\\.yml", ".json", my_files[i])
+    writeLines(x, newname) 
+  }
+  my_files <- gsub("\\.yaml", ".json", my_files)
+  output <- lapply(my_files, function(z) read_json(z, simplifyVector=TRUE))
+  expect_true(all(unlist(lapply(output, class)) == "data.frame"))
+  # rbind.pages combines a list of dataframes
+  output <- jsonlite::rbind_pages(output)
+  output <- vectorize(output)
+  expect_true(all(dim(output) == c(21,4)))
+})
+
+
+
+
 
